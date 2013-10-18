3893b0d236782ba011435d4a460922ec7314b2e5
spanner888 <spanner888@usabledevices.com>
2013-10-18 14:23:25 +0800

    added some more web pages to show, some with Chinese.

--- TheSpaceProjectS_Info/TheSpaceProjectS_Info.ino
+++ TheSpaceProjectS_Info/TheSpaceProjectS_Info.ino
@@ -113,14 +113,14 @@ char* cmdLineS[] = {
   "echo hack me!!!",
 
   //page #20
-  "echo p20",
-  "echo p21",
-  "echo p22",
-  "echo p23",
-  "echo p24",
+  "/usr/bin/chromium-browser '/home/ubuntu/Desktop/www/Aaaiiirrr.org - XinCheJian.html",
+  "/usr/bin/chromium-browser '/home/ubuntu/Desktop/www/ArduBlock - XinCheJian.html",
+  "/usr/bin/chromium-browser '/home/ubuntu/Desktop/www/Helper Sessions - XinCheJian.html",
+  "/usr/bin/chromium-browser '/home/ubuntu/Desktop/www/Assistive Devices - XinCheJian.html",
+  "/usr/bin/chromium-browser '/home/ubuntu/Desktop/www/TheSpaceProjectS - XinCheJian.html",
 
   //page #25
-  "echo p25",
+  "/usr/bin/chromium-browser '/home/ubuntu/Desktop/www/Urban Farming - XinCheJian.html",
   "echo p26",
   "echo p27",
   "echo p28",
